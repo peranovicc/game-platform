b700cbbacfa305912a0418e1a29f4b5307231fd5
peranovicc <cvijanperanovic@aol.com>
2020-04-03 11:57:28 +0200

    Updates

---
+++ precache-manifest.9789b84787097488a8e1738bdc7aeb79.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5359e7f4c1275f17df5febb92d0e0ea7",
+    "url": "/game-platform/index.html"
+  },
+  {
+    "revision": "a5eab8cc441a448f9b57",
+    "url": "/game-platform/static/css/2.c0851dc3.chunk.css"
+  },
+  {
+    "revision": "0120b6b4eb0785283b67",
+    "url": "/game-platform/static/css/main.56affd72.chunk.css"
+  },
+  {
+    "revision": "a5eab8cc441a448f9b57",
+    "url": "/game-platform/static/js/2.04551e6e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/game-platform/static/js/2.04551e6e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0120b6b4eb0785283b67",
+    "url": "/game-platform/static/js/main.b2221dfe.chunk.js"
+  },
+  {
+    "revision": "a571db1af8f1d396d1a4",
+    "url": "/game-platform/static/js/runtime-main.163a9d19.js"
+  }
+]);
